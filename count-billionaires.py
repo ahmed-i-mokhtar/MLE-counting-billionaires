0daf0b41bc8f4b17b021a0779ebd65e3532840d7
aimokhtar <ahmed.ahmed969@eng-st.cu.edu.eg>
2020-05-25 01:40:09 +0200

    Updated Read Me

---
+++ count-billionaires.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from numpy import exp
+import matplotlib.pyplot as plt
+
+from scipy.special import factorial
+import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
+import statsmodels.api as sm
+from statsmodels.api import Poisson
+from scipy import stats
+from scipy.stats import norm
+from statsmodels.iolib.summary2 import summary_col
+
+
+pd.options.display.max_columns = 10
+
+# Load in data and view
+df = pd.read_stata('https://github.com/QuantEcon/lecture-source-py/blob/master/source/_static/lecture_specific/mle/fp.dta?raw=true')
+print(df.head())
+
+numbil0_2008 = df[(df['year'] == 2008) & (
+    df['country'] != 'United States')].loc[:, 'numbil0']
+
+plt.subplots(figsize=(12, 8))
+plt.hist(numbil0_2008, bins=30)
+plt.xlim(left=0)
+plt.grid()
+plt.xlabel('Number of billionaires in 2008')
+plt.ylabel('Count')
+print("The plot proves that the best probability distribution is a poisson.")
+plt.show()
+
+
+
